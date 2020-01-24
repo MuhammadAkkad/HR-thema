a8c83e6b767278e7ed2f0fe77fce5dab473ae94f
Muahammad <mhammad.akkad@gmail.com>
2020-01-24 07:23:27 +0300

    slider page init

--- app/src/main/java/com/example/hr_thema/MainActivity.java
+++ app/src/main/java/com/example/hr_thema/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.hr_thema;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import com.example.hr_thema.slider.Slider;
+import com.smarteist.autoimageslider.IndicatorAnimations;
+import com.smarteist.autoimageslider.IndicatorView.draw.controller.DrawController;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
+
 public class MainActivity extends AppCompatActivity {
+    SliderView sliderView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sliderView = findViewById(R.id.imageSlider);
+
+        final Slider adapter = new Slider(this);
+        adapter.setCount(3);
+
+        sliderView.setSliderAdapter(adapter);
+
+        sliderView.setIndicatorAnimation(IndicatorAnimations.SLIDE); //set indicator animation by using SliderLayout.IndicatorAnimations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
+        sliderView.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
+        sliderView.setAutoCycleDirection(SliderView.AUTO_CYCLE_DIRECTION_BACK_AND_FORTH);
+        sliderView.setIndicatorSelectedColor(Color.WHITE);
+        sliderView.setIndicatorUnselectedColor(Color.GRAY);
+        sliderView.setAutoCycle(false);
+
+        sliderView.setOnIndicatorClickListener(new DrawController.ClickListener() {
+            @Override
+            public void onIndicatorClicked(int position) {
+                sliderView.setCurrentPagePosition(position);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/hr_thema/slider/Slider.java
@@ -0,0 +1,117 @@
+package com.example.hr_thema.slider;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.example.hr_thema.R;
+import com.smarteist.autoimageslider.SliderViewAdapter;
+
+public class Slider extends
+        SliderViewAdapter<Slider.SliderAdapterVH> {
+
+    private Context context;
+    private int mCount;
+
+    public Slider(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public SliderAdapterVH onCreateViewHolder(ViewGroup parent) {
+        View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.image_slider_layout_item, null);
+        return new SliderAdapterVH(inflate);
+    }
+
+    @Override
+    public void onBindViewHolder(SliderAdapterVH viewHolder, final int position) {
+
+
+        viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "This is item in position " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        switch (position) {
+            case 0:
+                viewHolder.textViewHeader.setText("Lorem Ipsum, dizgi");
+                viewHolder.textViewDescription.setText("Lorem Ipsum, dizgi ve baskı endüstrisi\n" +
+                        "kullanılan mıgır metinlerdir. ");
+                Glide.with(viewHolder.itemView)
+                        .load("https://images.pexels.com/photos/218983/pexels-photo-218983.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
+                        .fitCenter()
+                        .into(viewHolder.imageViewBackground);
+                break;
+            case 1:
+                viewHolder.textViewHeader.setText("Lorem Ipsum, dizgi");
+                viewHolder.textViewDescription.setText("Lorem Ipsum, dizgi ve baskı endüstrisi\n" +
+                        "kullanılan mıgır metinlerdir. ");
+                Glide.with(viewHolder.itemView)
+                        .load("https://images.pexels.com/photos/747964/pexels-photo-747964.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260")
+                        .fitCenter()
+                        .into(viewHolder.imageViewBackground);
+                break;
+            case 2:
+                viewHolder.textViewHeader.setText("Lorem Ipsum, dizgi");
+                viewHolder.textViewDescription.setText("Lorem Ipsum, dizgi ve baskı endüstrisi\n" +
+                        "kullanılan mıgır metinlerdir. ");
+                Glide.with(viewHolder.itemView)
+                        .load("https://images.pexels.com/photos/929778/pexels-photo-929778.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
+                        .fitCenter()
+                        .into(viewHolder.imageViewBackground);
+                break;
+            default:
+                viewHolder.textViewHeader.setText("Lorem Ipsum, dizgi");
+                viewHolder.textViewDescription.setText("Lorem Ipsum, dizgi ve baskı endüstrisi\n" +
+                        "kullanılan mıgır metinlerdir. ");
+                Glide.with(viewHolder.itemView);
+                       // .load(R.drawable.bg_overlay)
+                        //.fitCenter()
+                       // .into(viewHolder.imageViewBackground);
+
+               // .asGif()
+               // .load(R.drawable.oh_look_at_this)
+               // .into(viewHolder.imageGifContainer);
+                break;
+
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        //slider view count could be dynamic size
+        return mCount;
+    }
+
+    public void setCount(int count) {
+        this.mCount = count;
+    }
+
+    class SliderAdapterVH extends SliderViewAdapter.ViewHolder {
+
+        View itemView;
+        ImageView imageViewBackground;
+        TextView textViewHeader;
+        TextView textViewDescription;
+
+        public SliderAdapterVH(View itemView) {
+            super(itemView);
+            imageViewBackground = itemView.findViewById(R.id.iv_auto_image_slider);
+            textViewHeader = itemView.findViewById(R.id.tv_slider_header);
+            textViewDescription = itemView.findViewById(R.id.tv_slider_body);
+            this.itemView = itemView;
+        }
+    }
+
+
+}
