4fd327c60fccdf63f948ac2acd175d68d64ce01e
Muhammad AKKAD <mhammad.akkad@gmail.com>
2020-02-06 16:50:33 +0300

    nav bar open method

--- app/src/main/java/com/example/hr_thema/home/HomeFragment.java
+++ app/src/main/java/com/example/hr_thema/home/HomeFragment.java
@@ -9,16 +9,31 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.hr_thema.R;
+import com.example.hr_thema.navigationDrawer.IDrawerListener;
 
 public class HomeFragment extends Fragment {
 
+    ImageButton drawer_trigger;
     View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        drawer_trigger = view.findViewById(R.id.btn_trigger_drawer);
+        drawer_trigger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IDrawerListener drawerListener = (IDrawerListener) getActivity();
+                drawerListener.OpenDrawer();
+
+
+            }
+        });
 
         return view;
     }
