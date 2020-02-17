201368beef88ae925f666e6237786c9ea1dce5f4
Muhammad AKKAD <mhammad.akkad@gmail.com>
2020-02-17 14:41:11 +0300

    login page design update

--- app/src/main/java/com/example/hr_thema/login/LoginActivity.java
+++ app/src/main/java/com/example/hr_thema/login/LoginActivity.java
@@ -4,7 +4,11 @@ import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
 import android.os.Bundle;
+import android.text.TextPaint;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -22,13 +26,13 @@ import com.makeramen.roundedimageview.RoundedImageView;
 
 
 public class LoginActivity extends AppCompatActivity {
-    Button btn_sign_in,btnOtherAccount;
+    Button btn_sign_in;
     Dialog myDialog;
     Context context;
-    CheckBox chbRemember;
-    RoundedImageView rivProfilPhoto;
-    TextView txtWelcome,txtFormDescription,txtEMail,txtPassword,txtOr;
-    EditText edtEMail,edtPassword;
+    EditText txtEMail;
+    EditText edtPassword;
+    TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,68 +49,26 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        txtWelcome = findViewById(R.id.txt_welcome_activity_login);
-        txtFormDescription = findViewById(R.id.txt_welcome_description_activity_login);
-        txtEMail = findViewById(R.id.txt_user_name_activity_login);
-        txtPassword = findViewById(R.id.txt_password_activity_login2);
-        txtOr = findViewById(R.id.txt_yada_activity_login);
-        edtEMail = findViewById(R.id.tv_user_name_activity_login);
+
+        txtEMail = findViewById(R.id.tv_email_activity_login);
         edtPassword = findViewById(R.id.tv_password_activity_login);
-        btnOtherAccount = findViewById(R.id.btn_another_account_sign_in_activity_login);
-        rivProfilPhoto = findViewById(R.id.img_profile_activity_login);
-
-        chbRemember = findViewById(R.id.cb_remember_me_activity_login);
-
-        txtWelcome.setText("Hoşgeldin");
-        rivProfilPhoto.setVisibility(View.GONE);
-        txtEMail.setVisibility(View.VISIBLE);
-        edtEMail.setVisibility(View.VISIBLE);
-        txtPassword.setVisibility(View.VISIBLE);
-        txtOr.setVisibility(View.GONE);
-        btnOtherAccount.setVisibility(View.GONE);
-
-        edtPassword.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                txtPassword.setTextColor(ContextCompat.getColor(context, R.color.specialBlue));
-                txtEMail.setTextColor(ContextCompat.getColor(context, R.color.specialGray));
-            }
-        });
-
-       edtEMail.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-           @Override
-           public void onFocusChange(View v, boolean hasFocus) {
-               txtPassword.setTextColor(ContextCompat.getColor(context, R.color.specialGray));
-               txtEMail.setTextColor(ContextCompat.getColor(context, R.color.specialBlue));
-           }
-       });
 
 
-        chbRemember.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked == true){
-                    txtPassword.setTextColor(ContextCompat.getColor(context, R.color.specialBlue));
-                    txtWelcome.setText("Hoşgeldin,Emre");
-                    txtEMail.setVisibility(View.GONE);
-                    edtEMail.setVisibility(View.GONE);
-                    rivProfilPhoto.setVisibility(View.VISIBLE);
-                    txtOr.setVisibility(View.VISIBLE);
-                    btnOtherAccount.setVisibility(View.VISIBLE);
-                    txtPassword.setVisibility(View.VISIBLE);
-                }
-                else{
-                    txtPassword.setTextColor(ContextCompat.getColor(context, R.color.specialGray));
-                    txtWelcome.setText("Hoşgeldin");
-                    rivProfilPhoto.setVisibility(View.GONE);
-                    txtEMail.setVisibility(View.VISIBLE);
-                    edtEMail.setVisibility(View.VISIBLE);
-                    txtPassword.setVisibility(View.VISIBLE);
-                    txtOr.setVisibility(View.GONE);
-                    btnOtherAccount.setVisibility(View.GONE);
-                }
-            }
-        });
+        textView = (TextView) findViewById(R.id.textView5);
+
+        TextPaint paint = textView.getPaint();
+        float width = paint.measureText("Giriş Yap");
+
+        Shader textShader = new LinearGradient(0, 0, width, textView.getTextSize(),
+                new int[]{
+                        Color.parseColor("#CF8FDC"),
+                        Color.parseColor("#D691C1"),
+                        Color.parseColor("#E1939E"),
+                        Color.parseColor("#ED9576"),
+                        Color.parseColor("#F39663"),
+                }, null, Shader.TileMode.CLAMP);
+        textView.getPaint().setShader(textShader);
+
     }
     void home() {
         Intent home = new Intent(this, NavigationDrawer.class);
