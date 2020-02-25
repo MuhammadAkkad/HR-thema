d3d66e6bb8f7deb8365231480f06696a9c5ae4ec
Muhammad AKKAD <mhammad.akkad@gmail.com>
2020-02-25 14:24:04 +0300

    commit

--- app/src/main/java/com/example/hr_thema/login/LoginActivity.java
+++ app/src/main/java/com/example/hr_thema/login/LoginActivity.java
@@ -41,12 +41,11 @@ public class LoginActivity extends AppCompatActivity {
     Button btn_sign_in;
     Dialog myDialog;
     Context context;
-    EditText txtEMail;
+    EditText edtEMail;
     EditText edtPassword;
     TextView textView;
     TextView forgetPassword;
     static String deneme;
-    static Void denemeVoid;
     String accessToken;
 
     @Override
@@ -54,16 +53,13 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Token fakeToken = new Token();
-        fakeToken.setAccessToken("Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJib3JhbiIsImp0aSI6ImE2OTE4OTFlLTU0YmEtNGM1Yi1hNGZiLTRmODRhMDM0MmFiZSIsImlhdCI6MTU4MjUzMTU4NSwiVXNlckRhdGEiOiJ7XCJJZFwiOjgsXCJOYW1lXCI6XCJCb3JhblwiLFwiU3VybmFtZVwiOlwiR29rYnVsdXRcIixcIlVzZXJOYW1lXCI6XCJib3JhblwiLFwiRnVsbE5hbWVcIjpcIkJvcmFuIEdva2J1bHV0XCIsXCJQaG90b3VybFwiOlwiL0NvbnRlbnQvaW1hZ2VzL2VtcHktdXNlci5wbmdcIixcIkxvZ2luVHlwZXNcIjpudWxsLFwiUm9sZXNcIjpbe1wiQ29kZVwiOm51bGwsXCJDb2RlMlwiOm51bGwsXCJOYW1lXCI6bnVsbCxcIkRlc2NyaXB0aW9uXCI6bnVsbCxcIkRlc2NyaXB0aW9uMlwiOm51bGwsXCJSZWZLZXlcIjpudWxsLFwiUmVmS2V5MlwiOjAsXCJSZWZLZXkzXCI6bnVsbCxcIlJlZktleTRcIjowLFwiUmVmS2V5NVwiOlwiMDAwMDAwMDAtMDAwMC0wMDAwLTAwMDAtMDAwMDAwMDAwMDAwXCIsXCJSZWZLZXk2XCI6ZmFsc2UsXCJSZWZLZXlGbG9hdFwiOjAuMCxcIlJlZkRhdGVcIjpcIjAwMDEtMDEtMDFUMDA6MDA6MDBcIixcIlJlZkRhdGUyXCI6XCIwMDAxLTAxLTAxVDAwOjAwOjAwXCIsXCJSZWZEYXRlM1wiOm51bGwsXCJMaXN0XCI6bnVsbCxcIkludExpc3RcIjpbXSxcIlNlbGV0ZWRDb3VudFwiOm51bGwsXCJJc2NvbXBsZXRlZFwiOmZhbHNlLFwiU3RhdHVcIjpmYWxzZSxcIlJlZktleURvdWJsZVwiOjAuMCxcIlRpdGxlXCI6bnVsbCxcIkhhc1dhcm5pbmdNZXNzYWdlXCI6ZmFsc2UsXCJUYWdDb2xvckNvZGVcIjpudWxsLFwiSXNOb3JtXCI6ZmFsc2UsXCJDYW5Vc2VBc0xpbmtcIjpmYWxzZSxcIkxldmVsXCI6MCxcIlN1YkxldmVsXCI6MCxcIk5vcm1TdGFmZkRvbWFpblVzZXJJZFwiOjAsXCJQaG90b1wiOm51bGwsXCJPcmRlclwiOjAsXCJSZWZEYXRlRm9ybWF0XCI6XCIwMS4wMS4wMDAxXCIsXCJUZXh0XCI6bnVsbCxcIlBhcmFtZXRlckxpc3RcIjpbXSxcIkRvbWFpblVzZXJJZFwiOjAsXCJUYWdMaXN0XCI6bnVsbCxcIkludExpc3QyXCI6bnVsbCxcIkNvdW50XCI6MCxcIkN1cnJlbmN5RW51bVwiOjAsXCJJc0RlZmF1bHRcIjpmYWxzZSxcIkNvbG9yXCI6bnVsbCxcIkljb25cIjpudWxsLFwiSWRcIjoxLFwiSXNTZWxlY3RlZFwiOmZhbHNlLFwiSXNBY3RpdmVcIjpmYWxzZSxcIklzRGVsZXRlZFwiOmZhbHNlLFwiQ29tbWVudENvdW50XCI6MCxcIkFkZGVkQnlcIjowLFwiQWRkZWRCeUlkXCI6bnVsbCxcIkNyZWF0ZWREYXRlXCI6bnVsbH0se1wiQ29kZVwiOm51bGwsXCJDb2RlMlwiOm51bGwsXCJOYW1lXCI6bnVsbCxcIkRlc2NyaXB0aW9uXCI6bnVsbCxcIkRlc2NyaXB0aW9uMlwiOm51bGwsXCJSZWZLZXlcIjpudWxsLFwiUmVmS2V5MlwiOjAsXCJSZWZLZXkzXCI6bnVsbCxcIlJlZktleTRcIjowLFwiUmVmS2V5NVwiOlwiMDAwMDAwMDAtMDAwMC0wMDAwLTAwMDAtMDAwMDAwMDAwMDAwXCIsXCJSZWZLZXk2XCI6ZmFsc2UsXCJSZWZLZXlGbG9hdFwiOjAuMCxcIlJlZkRhdGVcIjpcIjAwMDEtMDEtMDFUMDA6MDA6MDBcIixcIlJlZkRhdGUyXCI6XCIwMDAxLTAxLTAxVDAwOjAwOjAwXCIsXCJSZWZEYXRlM1wiOm51bGwsXCJMaXN0XCI6bnVsbCxcIkludExpc3RcIjpbXSxcIlNlbGV0ZWRDb3VudFwiOm51bGwsXCJJc2NvbXBsZXRlZFwiOmZhbHNlLFwiU3RhdHVcIjpmYWxzZSxcIlJlZktleURvdWJsZVwiOjAuMCxcIlRpdGxlXCI6bnVsbCxcIkhhc1dhcm5pbmdNZXNzYWdlXCI6ZmFsc2UsXCJUYWdDb2xvckNvZGVcIjpudWxsLFwiSXNOb3JtXCI6ZmFsc2UsXCJDYW5Vc2VBc0xpbmtcIjpmYWxzZSxcIkxldmVsXCI6MCxcIlN1YkxldmVsXCI6MCxcIk5vcm1TdGFmZkRvbWFpblVzZXJJZFwiOjAsXCJQaG90b1wiOm51bGwsXCJPcmRlclwiOjAsXCJSZWZEYXRlRm9ybWF0XCI6XCIwMS4wMS4wMDAxXCIsXCJUZXh0XCI6bnVsbCxcIlBhcmFtZXRlckxpc3RcIjpbXSxcIkRvbWFpblVzZXJJZFwiOjAsXCJUYWdMaXN0XCI6bnVsbCxcIkludExpc3QyXCI6bnVsbCxcIkNvdW50XCI6MCxcIkN1cnJlbmN5RW51bVwiOjAsXCJJc0RlZmF1bHRcIjpmYWxzZSxcIkNvbG9yXCI6bnVsbCxcIkljb25cIjpudWxsLFwiSWRcIjozLFwiSXNTZWxlY3RlZFwiOmZhbHNlLFwiSXNBY3RpdmVcIjpmYWxzZSxcIklzRGVsZXRlZFwiOmZhbHNlLFwiQ29tbWVudENvdW50XCI6MCxcIkFkZGVkQnlcIjowLFwiQWRkZWRCeUlkXCI6bnVsbCxcIkNyZWF0ZWREYXRlXCI6bnVsbH1dLFwiUGhvbmVcIjpcIjUzNDM1MzUzXCIsXCJFbWFpbFwiOlwiOF9iZXloYW4udHV0dW5jdWxlckBiaWxnZWFkYW0uY29tXCIsXCJUaXRsZVwiOlwiRGlyZWt0w7ZyIFwifSIsIm5iZiI6MTU4MjUzMTU4NSwiZXhwIjoxNTgyNzA0Mzg1LCJpc3MiOiJIQ00uQXBpIiwiYXVkIjoiSENNLkFwaSJ9.Jfewv8bIdZj7x6jYoqOvVTrnCBWcb9klyKY1ow7_31U");
-
         btn_sign_in = findViewById(R.id.btn_sign_in_activity_login);
         context = this;
         myDialog = new Dialog(this);
         btn_sign_in.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                gam();
+                loginControlWebAPI();
             }
         });
 
@@ -75,10 +71,8 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        txtEMail = findViewById(R.id.tv_email_activity_login);
+        edtEMail = findViewById(R.id.tv_email_activity_login);
         edtPassword = findViewById(R.id.tv_password_activity_login);
-
-
         textView = findViewById(R.id.textView5);
 
         TextPaint paint = textView.getPaint();
@@ -93,46 +87,29 @@ public class LoginActivity extends AppCompatActivity {
                         Color.parseColor("#F39663"),
                 }, null, Shader.TileMode.CLAMP);
         textView.getPaint().setShader(textShader);
-
-
-
     }
 
     //API
-    public void gam() {
-        BodyParameters bodyParameters = new BodyParameters();
-        bodyParameters.setUserName("boran");
-        bodyParameters.setPassword("123");
-        bodyParameters.setGrant_type("password");
-
-        Gson gson = new GsonBuilder().setLenient().create();
+    public void loginControlWebAPI() {
         Retrofit retrofit = new Retrofit.Builder().baseUrl(APIUrl.BASE_URL).addConverterFactory(GsonConverterFactory.create()).client(new OkHttpClient()).build();
         APIService apis = retrofit.create(APIService.class);
-        Call<Token> call = apis.getDataToken("application/x-www-form-urlencoded",txtEMail.getText().toString(),edtPassword.getText().toString(),"password");
+        Call<Token> call = apis.getDataToken("application/x-www-form-urlencoded",edtEMail.getText().toString(),edtPassword.getText().toString(),"password");
 
         call.enqueue(new Callback<Token>() {
             @Override
             public void onResponse(Call<Token> call, Response<Token> response) {
                 if(response.isSuccessful()){
+                    accessToken = response.body().getAccessToken();
                     Intent i = new Intent(getApplicationContext(),NavigationDrawer.class);
                     startActivity(i);
-                    System.out.println("başarılı");
-                    System.out.println(response.body().getAccessToken());
-                    accessToken = response.body().getAccessToken();
-                    System.out.println(response.body().getUserName());
-                    System.out.println(response.body().getIssued());
                 }
                 else{
-                    System.out.println("başarısız");
                     Toast.makeText(getApplicationContext(),"Girdiğiniz şifre veya kullanıcı adı hatalı!",Toast.LENGTH_LONG).show();
                 }
-                //deneme = response.body().getAccessToken();
-                //Toast.makeText(getApplicationContext()," adsf" + response.body().getAccessToken(),Toast.LENGTH_LONG).show();
             }
 
             @Override
             public void onFailure(Call<Token> call, Throwable t) {
-                System.out.println(t + " adsf");
                 Toast.makeText(getApplicationContext()," onFailure",Toast.LENGTH_LONG).show();
             }
         });
@@ -147,8 +124,6 @@ public class LoginActivity extends AppCompatActivity {
         call.enqueue(new Callback<User>() {
             @Override
             public void onResponse(Call<User> call, Response<User> response) {
-//                Token getToken = (Token) response.body();
-//                getToken.getAccessToken();
                 deneme = response.body().getFullName();
                 if(response.isSuccessful() == true){
                     System.out.println("başarılı");
@@ -157,8 +132,6 @@ public class LoginActivity extends AppCompatActivity {
                 else{
                     System.out.println("başarısız");
                 }
-                //deneme = response.body().getAccessToken();
-                //Toast.makeText(getApplicationContext()," adsf" + response.body().getAccessToken(),Toast.LENGTH_LONG).show();
             }
 
             @Override
@@ -167,11 +140,6 @@ public class LoginActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext()," onFailure" + t,Toast.LENGTH_LONG).show();
             }
         });
-    }
-
-    void home() {
-        Intent home = new Intent(this, NavigationDrawer.class);
-        startActivity(home);
     }
 
     void LoggedInUser(){
